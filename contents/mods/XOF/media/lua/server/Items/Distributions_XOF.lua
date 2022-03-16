1007735720dac205cd4bdc49841eed84feb9950f
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-03-15 22:54:16 -0400

    Initial Commit

---
+++ contents/mods/XOF/media/lua/server/Items/Distributions_XOF.lua
@@ -0,0 +1,6 @@
+require 'Items/ProceduralDistributions'
+
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "XOF.Vest_XOFVest");
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 0.3);
+
+
