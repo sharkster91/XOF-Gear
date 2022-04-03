0c8945f9c993d1006dd5a9662c8dacdb9d6c12ed
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-04-02 20:00:52 -0400

    Added Balaclava + tweaked suit + operator

--- contents/mods/XOF/media/lua/client/XOFOperator.lua
+++ contents/mods/XOF/media/lua/client/XOFOperator.lua
@@ -12,10 +12,13 @@ ProfessionFramework.addProfession('XOF', {
 
     inventory = {
 		["XOF.Hat_XOFHelmet"] = 1,
+    ["XOF.Hat_XOFHelmetNVGUP"] = 1,
     ["XOF.Glasses_XOFGoggles"] = 1,
 		["Base.Shoes_ArmyBoots"] = 1,
 		["XOF.Vest_XOFVest"] = 1,
 		["XOF.XOFArmArmor"] = 1,
     ["XOF.Gloves_XOFGloves"] = 1,
+    ["XOF.XOFBalaclava"] = 1,
+    ["XOF.XOFSuit"] = 1,
     },
 })
